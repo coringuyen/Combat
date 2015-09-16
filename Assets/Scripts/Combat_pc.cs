cf52d4d11d061e176fa74a062aae8bc492b631a4
Chui <lordbabypanda@gmail.com>
2015-09-16 18:11:21 -0500

    Ideas!
    Thoughts!

---
+++ Assets/Scripts/Combat_pc.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Combat_pc : MonoBehaviour
+{
+    // Delegates
+    // coroutines
+    // Never use Update
+    // Verisoned.
+    // Simple Combat game
+    // modlar. can be used in other games.
+    // Animation, your own translation and at least 6 states.
+
+    delegate void Combat()
+    Combat combat;
+    
+    
+	void Start ()
+    {
+	
+	}
+	
+
+}
